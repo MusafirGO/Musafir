9b09011f42b136cd4a3cd8e97f6ef14f4befec2f
loyal <loyal>
2017-03-25 00:43:15 +0530

    Edited src and res

---
+++ src/MusicController.java
@@ -0,0 +1,19 @@
+package objects;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+
+
+
+
+}
